216f70dd754cd0ae8a80621b12cb74ac7973545a
Denis Carriere <carriere.denis@gmail.com>
2023-05-28 20:36:21 -0400

    add signature

--- src/activities.ts
+++ src/activities.ts
@@ -1,12 +1,20 @@
 import { Clock } from "substreams";
+import { PrivateKey, Bytes } from "@wharfkit/session";
 
 export async function postWebhook(message: any, clock: Clock, url: string, privateKey: string) {
   const body = JSON.stringify({message, clock});
-  console.log("POST", url);
-  // const response = await fetch(url, {
-  //   body,
-  //   method: 'POST',
-  //   headers: {'Content-Type': 'application/json'}
-  // });
-  return true;
+  const timestamp = String(Math.floor(Date.now().valueOf() / 1000));
+  const hex = Buffer.from(body).toString("hex");
+  const bytes = Bytes.from(hex);
+  const signature = PrivateKey.fromString(privateKey).signMessage(bytes).toString();
+
+  return fetch(url, {
+    body,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Signature-Secp256k1': signature,
+      'X-Signature-Timestamp': timestamp,
+    }
+  });
 }
