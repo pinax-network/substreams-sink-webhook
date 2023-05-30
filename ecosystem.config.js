9749b416f355016e887d3a60b07a4f757c4be77f
Denis Carriere <carriere.denis@gmail.com>
2023-05-30 11:39:43 -0400

    fix: run as node

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 const server = {
   name: "server",
-  script: "tsx ./bin/cli.ts server",
+  script: "node ./dist/bin/cli.js server",
 }
 
 const worker = {
   name: "worker",
-  script: "tsx ./bin/cli.ts worker",
+  script: "node ./dist/bin/cli.js worker",
 }
 
 const run = {
   name: "run",
-  script: "tsx ./bin/cli.ts run https://github.com/pinax-network/substreams/releases/download/eosio.token-v0.12.1/eosio-token-v0.12.1.spkg map_transfers -e https://wax.firehose.eosnation.io:9001 --production-mode -s -1 --verbose",
+  script: "node ./dist/bin/cli.js run https://github.com/pinax-network/substreams/releases/download/eosio.token-v0.12.1/eosio-token-v0.12.1.spkg map_transfers -e https://wax.firehose.eosnation.io:9001 --production-mode -s -1 --verbose",
 }
 
 module.exports = {
