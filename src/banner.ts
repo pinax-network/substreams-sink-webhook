781c7aa059520d9cea81284accbdc69a84c49ae7
Denis Carriere <carriere.denis@gmail.com>
2024-02-13 15:47:02 -0500

    format

--- src/banner.ts
+++ src/banner.ts
@@ -13,8 +13,8 @@ export function banner() {
 
 `;
   text += `                      ${pkg.name} v${pkg.version}\n\n`;
-  text += `HTTP routes\n`;
-  text += `GET /metrics\tPrometheus Metrics\n`;
-  text += `GET /keypair\tGenerate random Ed25519 private & public keys\n`;
+  text += "HTTP routes\n";
+  text += "GET /metrics\tPrometheus Metrics\n";
+  text += "GET /keypair\tGenerate random Ed25519 private & public keys\n";
   return text;
 }
